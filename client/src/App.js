7d7e45c53acaed84c786817f76638a6e668f3abe
Jeremy Stocks <jeremy.stocks@zoho.com>
2021-05-26 21:36:42 +0200

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import Book from './Book'
+import './App.css';
 
 
 function App() {
 
-  const greeting = <div>Hello World!!!!</div>;
+
   return (
     <div className="App">
-      {greeting}
+     <Book />
     </div>
   );
 }
