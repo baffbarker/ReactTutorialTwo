0dbf97ab200a20da5affcd5031f084a290fab087
Jeremy Stocks <jeremy.stocks@zoho.com>
2021-06-01 21:32:26 +0200

    pool

--- server/routes/books.js
+++ server/routes/books.js
@@ -2,34 +2,30 @@ const express = require('express');
 const mysql = require('mysql')
 
 const router = express.Router();
+const connectionPool = require('../database/connection-pool')
 
 
-
-const db = require('../config/db');
-
-console.log(db.user, db.password);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  const conn = mysql.createConnection(db);
 
-  conn.connect(err => {
-    if(err) throw err;
+
+
 
     const book = {
       'author': 'Charles Dickens',
-      'title': 'Great Expectations',
+      'title': 'Awful Expectations',
       'published': '1861-01-01'
     };
 
-    conn.query('insert into books set ?', book, (err, result) => {
+    connectionPool.getPool().query('insert into books set ?', book, (err, result) => {
       if(err) throw err;
 
       console.log(result)
 
     });
-  });
+
 
   res.send('books here!');
 });
